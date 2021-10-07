d9253052ebbade0ae0f97fd5ab86792020cbf67a
Joseph Lawrence <joseph.lawrence-2@student.manchester.ac.uk>
2021-10-07 13:47:01 +0100

    Add object relational mappings

---
+++ domain/objects.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, Float, String
+
+Base = declarative_base()
+
+
+class Game(Base):
+    __tablename__ = 'game'
+
+    id = Column(Integer, primary_key=True)
+    white_level = Column(Integer)
+    black_level = Column(Integer)
+    outcome = Column(String)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class SingleMove(Base):
+    __tablename__ = 'single_move'
+
+    id = Column(Integer, primary_key=True)
+    gain = Column(Float)
+    starting_fen = Column(String)
+    ending_fen = Column(String)
+    type = Column(String)
+    move = Column(String)
+    to_move = Column(String)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class MateInN(Base):
+    __tablename__ = 'mate_in_n'
+
+    id = Column(Integer, primary_key=True)
+    starting_fen = Column(String)
+    to_move = Column(String)
+    moves_to_mate = Column(Integer)
+    game_id = Column(Integer)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
