20d13af77b0d97c4af4452a754a1e4bdbf1e51f2
Joe Lawrence <joelawrence121@hotmail.co.uk>
2021-11-02 09:54:22 +0000

    Move db_setup into chess-jenny

--- data/db_setup.sql
+++
@@ -1,9 +0,0 @@
-create table opening
-(
-    id int auto_increment primary key,
-	eco_classification varchar(5) not null,
-	name varchar(1024) not null,
-	move_stack varchar(1024) not null,
-	epd varchar(1024) not null
-);
-
